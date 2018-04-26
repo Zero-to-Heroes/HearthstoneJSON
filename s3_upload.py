49a3b17808bf6865285543b909e3503879c6f026
Jerome Leclanche <jerome@leclan.ch>
2018-04-26 07:09:39 +0300

    Remove 404 redirects from art.hearthstonejson.com

--- s3_upload.py
+++ s3_upload.py
@@ -39,44 +39,6 @@ def update_website_configuration(s3, build, bucket=API_BUCKET):
 		print("Website configuration up-to-date")
 
 
-def update_art_404_redirects(s3, bucket=ART_BUCKET):
-	orig_config = s3.get_bucket_website(Bucket=bucket)
-
-	if "ResponseMetadata" in orig_config:
-		del orig_config["ResponseMetadata"]
-
-	config = orig_config.copy()
-
-	prefixes = [
-		("v1/orig/", "png", "XXX_001"),
-		("v1/tiles/", "png", "HERO_01"),
-		("v1/256x/", "jpg", "XXX_001"),
-		("v1/512x/", "jpg", "XXX_001"),
-	]
-
-	config["RoutingRules"] = []
-
-	for prefix, ext, fallback in prefixes:
-		config["RoutingRules"].append({
-			"Condition": {
-				"HttpErrorCodeReturnedEquals": "404",
-				"KeyPrefixEquals": prefix,
-			},
-			"Redirect": {
-				"ReplaceKeyWith": prefix + "%s.%s" % (fallback, ext),
-				"HttpRedirectCode": "302",
-				"Protocol": "https",
-			}
-		})
-
-	if config != orig_config:
-		print("Updating 404 redirects")
-		pprint(config)
-		s3.put_bucket_website(Bucket=bucket, WebsiteConfiguration=config)
-	else:
-		print("404 redirects up-to-date")
-
-
 def main():
 	parser = ArgumentParser()
 	parser.add_argument("--build", type=int, nargs=1)
@@ -86,7 +48,6 @@ def main():
 	s3 = boto3.client("s3")
 
 	update_website_configuration(s3, args.build[0])
-	update_art_404_redirects(s3)
 
 
 if __name__ == "__main__":
