da904b05cfb3988e97730f994823a7819c86d56b
sebastientromp <sebastien.tromp@gmail.com>
2024-02-13 20:05:01 +0100

    update sound effects for 193541

--- extract_audio.py
+++ extract_audio.py
@@ -2,6 +2,7 @@
 import json
 import os
 import sys
+import datetime
 from argparse import ArgumentParser
 from collections import Counter
 
@@ -27,6 +28,15 @@ locales = [
 ]
 
 import sys
+
+# save the original print function
+original_print = print
+
+def print_with_timestamp(*args, **kwargs):
+    original_print(f"[{datetime.datetime.now()}]", *args, **kwargs)
+
+# replace print function with the new one
+print = print_with_timestamp
 
 class Logger(object):
     def __init__(self, logFile):
@@ -58,9 +68,9 @@ def main():
 	args = p.parse_args(sys.argv[1:])
 	for root, dirs, files in os.walk(args.src):
 		for file_name in files:
-			# if "sound" not in file_name.lower():
-			# 	# print("skipping %s" % file_name)
-			# 	continue
+			if "sound" not in file_name.lower() and "audio" not in file_name.lower():
+				# print("skipping %s" % file_name)
+				continue
 
 			# generate file_path
 			file_path = os.path.join(root, file_name)
@@ -71,8 +81,8 @@ def main():
 			for loc in locales:
 				if ("_" + loc.lower() + "-") in file_path.lower():
 					current_loc = loc
-					print("current_loc %s" % current_loc)
-					print("file path %s" % file_path)
+					# print("current_loc %s" % current_loc)
+					# print("file path %s" % file_path)
 
 			extract_assets(env, current_loc)
 
@@ -128,10 +138,12 @@ def export_obj(path, obj, current_loc):
 
 		wav_file_name = f"out/sounds_wav/{current_loc}/{base_file_name}.wav"
 		ogg_file_name = f"out/sounds/{current_loc}/{base_file_name}.ogg"
-		print("base_file_name %s" % base_file_name)
-		print("wav_file_name %s" % wav_file_name)
-		print("locale %s" % current_loc)
-		print("path %s" % path)
+		# print("base_file_name %s" % base_file_name)
+		# print("wav_file_name %s" % wav_file_name)
+		# print("locale %s" % current_loc)
+		# print("path %s" % path)
+
+		# print("warning, this should be uncommented")
 		if os.path.exists(wav_file_name):
 			# print("\t file already exists, continuing")
 			continue
@@ -140,13 +152,18 @@ def export_obj(path, obj, current_loc):
 			with open(wav_file_name, "wb") as f:
 				print("extracting wav file %s from %s" % (wav_file_name, path))
 				f.write(data)
-
 			print("\t exporting ogg file %s" % ogg_file_name)
 			sound = AudioSegment.from_wav(wav_file_name)		
 			sound.export(ogg_file_name, format="ogg")
 		except Exception as e:
 			print("could not extract wav file %s from %s" % (wav_file_name, path))
 			print("exception %s " % e)
+
+		if os.path.exists(ogg_file_name):
+			print("\togg file %s successfully created" % ogg_file_name)
+		else:
+			print("\togg file %s not found" % ogg_file_name)
+			raise Exception("ogg file %s not found" % ogg_file_name)
 
 
 if __name__ == '__main__':
--- extract_ref_objects.py
+++ extract_ref_objects.py
@@ -191,6 +191,23 @@ nodes_to_parse = [
 
 ignored = []
 
+class Logger(object):
+    def __init__(self, logFile):
+        self.terminal = sys.stdout
+        self.log = open(logFile, "a")
+   
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass    
+
+sys.stdout = Logger("extract_ref_objects.log")
+
 def main():
 	p = ArgumentParser()
 	p.add_argument("src")
